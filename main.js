ab9a991f0e1c85d3411152832417d3b626d25af6
Lalit Mehra <laganmehra875@gmail.com>
2026-04-15 16:16:27 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Data Scientist", "Machine Learning Enthusiast", "Data Analyst"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
